10c36784a6442df1b9763fe676052e3d5cbefef6
MXNP <sam.jiang77@live.com.au>
2020-06-16 02:51:55 +1000

    Beta 0.1

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+var cH = $('#crosshair-h'),
+    cV = $('#crosshair-v');
+
+$(document).on('mousemove',function(e){
+    cH.css('top',e.pageY);
+    cV.css('left',e.pageX);
+});
+});
